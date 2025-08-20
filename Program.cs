8d44ea5afa6790530c7a3f5efc8c708608e8d397
Angel Nuw <Angel.Nuw@mssa.newhorizons.com>
2025-08-20 11:22:25 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace Assignment7._2._2
+{
+    internal class Program
+    {
+        static string ReverseVowles(string s)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+            char[] chars = s.ToCharArray();
+            int left = 0;
+            int right = chars.Length - 1;
+
+            while (left < right)
+            {
+                while (left < right && !vowels.Contains(chars[left]))
+                {
+                    left++;
+                }
+                while (left < right && !vowels.Contains(chars[right]))
+                {
+                    right--;
+                }
+                if (left < right)
+                {
+                    char temp = chars[left];
+                    chars[left] = chars[right];
+                    chars[right] = temp;
+                    left++;
+                    right--;
+                }
+            }
+
+            return new string(chars);
+        }
+        static void Main(string[] args)
+        {
+            // Example usage
+            string input = ("Hippopotomus");
+            string result = ReverseVowles(input);
+            Console.WriteLine(result); // Output: "Holle Werld!"
+        }
+    }
+}
